0655d53e179f1ef637d34885377c62ae8cbcf96e
TreeFall1 <andreevdaniil06an@gmail.com>
2025-11-09 22:02:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
   },
   env: {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL
   },
 };
 
