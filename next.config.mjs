f93f3b564b9f5005894a7350fff5aee4b1b7c44c
TreeFall1 <andreevdaniil06an@gmail.com>
2025-12-04 13:57:07 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-        // new URL("https://dewxobvbnyxflymvoyxk.supabase.co/**"),
       {
         protocol: 'https',
         hostname: 'dewxobvbnyxflymvoyxk.supabase.co',
         pathname: '**'
       }
     ],
-    // domains: ["dewxobvbnyxflymvoyxk.supabase.co"]
   },
   env: {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
